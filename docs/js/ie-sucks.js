80f928d20509951f1b91fb05d1996241e9cedf35
Patrick O'Brien <patrick.obrien@zywave.com>
2021-11-03 09:31:05 -0500

    feat: redirect

--- docs/js/ie-sucks.js
+++
@@ -1,7 +0,0 @@
-if (window.customElements) {
-  try {
-    window.customElements.define('ie-sucks', class extends HTMLElement {});
-  } catch {
-    // we don't care if this breaks, for whatever reason
-  }
-}
--- docs/stack-blitz/index.ts
+++
@@ -1,1 +0,0 @@
-import "@zywave/zui-bundle";
